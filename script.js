44adff107f28156b596806f290771c251c65812f
manufrom63 <100273523+manufrom63@users.noreply.github.com>
2022-04-18 22:31:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let value = document.querySelector("#ageboxagebox");
+let age = document.querySelector("#age")
+let div = document.querySelector("#magicbox")
+let check = document.querySelector("#estguillaume")
+
+if (check.v)
