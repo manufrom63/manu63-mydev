25e3cd53468f23b20c388925ba1a5d46cebe458e
manufrom63 <100273523+manufrom63@users.noreply.github.com>
2022-04-18 22:56:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 
-let value = document.querySelector("#ageboxagebox");
-let age = document.querySelector("#age")
-let div = document.querySelector("#magicbox")
-let check = document.querySelector("#estguillaume")
+let value = document.querySelector("#agebox");
+let age = document.querySelector("#age");
+let div = document.querySelector("#magicbox");
+let check = document.querySelector("#estguillaume");
+let button = document.querySelector("#go");
 
-if (check.v)
+
+
+button.addEventListener("click", (event)=>{
+    event.preventDefault();
+    div.classList.remove("hiden");
+    div.classList.add("centerflex")
+    if (check.checked)
+    {
+        age.innerHTML = parseInt(value.value) +1;
+    }
+    else
+    {
+        age.innerHTML = parseInt(value.value)
+    }
+})
